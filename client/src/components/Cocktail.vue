d657cceefcdc088b01d0744b614c6dba915d03bb
Filip <35558467+AntiMagneticGuy@users.noreply.github.com>
2023-01-06 16:53:38 +0100

    basic functionality

---
+++ client/src/components/Cocktail.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="cocktail">
+        <header>
+            <h3>{{ cName }}</h3>
+            <p>Rating: {{ rating }} / 10</p>
+        </header>
+        <div class="info">
+        <img :src="imgPath" width="250" height="250">
+        <p>{{ desc }}</p>
+        </div>
+    </div>
+
+</template>
+
+
+<script>
+
+export default{
+    props: {
+        cName: String,
+        cIngr: Array,
+        desc: String,
+        rating: 0,
+        imgPath: "",
+        },
+
+}
+
+</script>
+
+<style scoped>
+.cocktail{
+    border: 3px solid black;
+    padding: 20px;
+}
+.info {
+    border: 1px solid black;
+    padding: 10px;
+}
+</style>
---
+++ client/src/views/mainCocktail.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="cen">
+<h1 >Welcome to the cocktail website!</h1>
+</div>
+<div v-for="cock in cocktails">
+<Cocktail v-bind="cock"></Cocktail>
+</div>
+</template>
+
+
+<script>
+console.log("aaaha");
+import Cocktail from '../components/Cocktail.vue'
+export default{
+    data() {
+        return {
+        cocktails: [
+            {cName: "Blue Lagoon", desc: "Blue cocktail", rating: 7, imgPath: "https://bakeitwithlove.com/wp-content/uploads/2022/08/Blue-Lagoon-Cocktail-sq.jpg"},
+            { cName: "Sex on the Beach", desc: "fersken", rating: 9, imgPath: "https://realhousemoms.com/wp-content/uploads/Sex-on-the-Beach-RECIPE-CARD.jpg"},
+            
+        ],
+        }
+    },
+
+    components: {
+        Cocktail,
+    }
+
+}
+
+
+
+</script>
+
+<style scoped>
+.cen {
+    text-align: center;
+}
+div{
+    padding: 20px;
+}
+</style>
